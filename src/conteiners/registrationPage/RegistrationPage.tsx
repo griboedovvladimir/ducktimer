a3ac80d3d2e37c12e6f866ccb914027f8823cebf
griboedovvladimir <likecoffee@yandex.ru>
2019-01-23 08:38:32 +0300

    Start!

---
+++ src/conteiners/registrationPage/RegistrationPage.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class RegistrationPage extends Component {
+    render() {
+        return <div>Registration</div>
+    }
+}
+
+export default RegistrationPage;
