2caf9b15be6dfd47509a96ba70e0379d227c692a
griboedovvladimir <likecoffee@yandex.ru>
2020-06-21 01:20:32 +0300

    Add clock formater

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -11,81 +11,99 @@ import { RouteComponentProps } from 'react-router';
 import Popup from '../Popup/Popup';
 
 class Main extends Component<IStoreInterface & RouteComponentProps> {
-  public date = new Date();
-  public state: any = {
-    clock: this.date.toLocaleTimeString('en-GB'),
-  };
+    public date = new Date();
+    public state: any = {
+        clock: this.date.toLocaleTimeString( 'en-GB' ),
+        clock12: false,
+    };
 
-  constructor(props: any) {
-    super(props);
-    if (
-      !(
-        storageService.getTokenFromLocalStorage() ||
-        storageService.getTokenFromSessionStorage()
-      )
-    ) {
-      this.props.history.push('/');
+    constructor( props: any ) {
+        super( props );
+        if (
+            !(
+                storageService.getTokenFromLocalStorage() ||
+                storageService.getTokenFromSessionStorage()
+            )
+        ) {
+            this.props.history.push( '/' );
+        }
+        setInterval( () => {
+            this.startClock();
+        }, 1000 );
     }
-    setInterval(() => {
-      this.startClock();
-    }, 1000);
-  }
 
-  public componentDidMount() {}
+    public componentDidMount() {
+    }
 
-  public onLogOut = () => {
-    storageService.removeTokenFromLocalStorage();
-    storageService.removeTokenFromSessionStorage();
-    this.props.authorize({ authorize: undefined });
-    this.props.history.push('/');
-  };
+    public onLogOut = () => {
+        storageService.removeTokenFromLocalStorage();
+        storageService.removeTokenFromSessionStorage();
+        this.props.authorize( {authorize: undefined} );
+        this.props.history.push( '/' );
+    };
 
-  public startClock() {
-    let d = new Date();
-    this.setState({ ...this.state, clock: d.toLocaleTimeString('en-GB') });
-  }
+    public startClock() {
+        let d = new Date();
+        this.setState( {
+            ...this.state,
+            clock: this.state.clock12
+                ? d.toLocaleString( 'en-US', {
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: 'numeric',
+                    hour12: true
+                } ).split( ' ' )[ 0 ]
+                :
+                d.toLocaleTimeString( 'en-GB' )
+        } )
+        ;
+    }
 
-  public initTimers() {
-    return this.props.timer.timers.map(
-      (timer: { time: string; id: string }): React.ReactNode => {
-        return <Timer key={timer.id} id={timer.id} time={timer.time} />;
-      }
-    );
-  }
+    public initTimers() {
+        return this.props.timer.timers.map(
+            ( timer: { time: string; id: string } ): React.ReactNode => {
+                return <Timer key={timer.id} id={timer.id} time={timer.time}/>;
+            }
+        );
+    }
 
-  render() {
-    return (
-      <div className={this.props.currentTheme.theme}>
-        {/*<div className="effect-background">*/}
-        <ThemeSwitcher />
-        <div className="row1">
-          <div id="clock">
-            <span id="timeicon" className="icon2">
+    public onChangeClockLocale = (): void => {
+        this.setState( {...this.state, clock12: !this.state.clock12} );
+    }
+
+    render() {
+        return (
+            <div className={this.props.currentTheme.theme}>
+                {/*<div className="effect-background">*/}
+                <ThemeSwitcher/>
+                <div className="row1">
+                    <div id="clock">
+            <span id="timeicon" className="icon2" onClick={this.onChangeClockLocale}>
               {' '}
-              
+                
             </span>
-            <div id="time">{this.state.clock}</div>
-          </div>
-          <div className="logout">
-            <button title="Log out" className="icon" onClick={this.onLogOut}>
-              
-            </button>
-          </div>
-        </div>
-        <TopMenu />
-        <div className="row2">
-          <RightMenu />
-          <div className="table">{this.initTimers()}</div>
-        </div>
-        {/*</div>*/}
-        {/*<Popup/>*/}
-      </div>
-    );
-  }
+                        <div id="time">{this.state.clock}</div>
+                    </div>
+                    <div className="logout">
+                        <button title="Log out" className="icon" onClick={this.onLogOut}>
+                            
+                        </button>
+                    </div>
+                </div>
+                <TopMenu/>
+                <div className="row2">
+                    <RightMenu/>
+                    <div className="table">{this.initTimers()}</div>
+                </div>
+                {/*</div>*/}
+                {/*<Popup/>*/}
+            </div>
+        );
+    }
 }
 
-const mapStateToProps = (state: any) => state;
+const mapStateToProps = ( state: any ) => state;
 export default connect(
-  mapStateToProps,
-  { ...actions }
-)(Main);
+    mapStateToProps,
+    {...actions}
+)( Main );
--- src/components/RightMenu/RightMenu.tsx
+++ src/components/RightMenu/RightMenu.tsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 
 class RightMenu extends Component<{}> {
+  public openPopup = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(e.currentTarget.id)
+  }
+
   render() {
     return (
       <div className="rightpanel">
         <div className="panel">
-          <button title="Volume mixer" className="icon2">
+          <button title="Volume mixer" id="widgetMixer" className="icon2" onClick={this.openPopup}>
             
           </button>
-          <button title="Time/temp converter" className="icon2">
+          <button title="Time/temp converter" id="tempConverter" className="icon2"  onClick={this.openPopup}>
             
           </button>
-          <button title="Push processing" className="icon2">
+          <button title="Push processing" id="pushProcessing" className="icon2" onClick={this.openPopup}>
             
           </button>
         </div>
